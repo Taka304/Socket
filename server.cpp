29126530e0b3967803b2e0aea78ea7319a0c4aec
Tranmyhan <67772342+Tranmyhan@users.noreply.github.com>
2020-12-20 17:22:00 +0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 		}
 		CSocket serverSocket;
 		//Tao socket lang nghe
-		if (serverSocket.Create(8080, SOCK_STREAM, NULL) == 0)
+		if (serverSocket.Create(80, SOCK_STREAM, NULL) == 0)
 		{
 			cout << "Tao socket that bai" << endl;
 			cout << serverSocket.GetLastError();
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 					Connector.Close();
 					serverSocket.Close();
 					CSocket serverSocket1,serverSocket2,con3;
-					if (serverSocket1.Create(8080, SOCK_STREAM, NULL) == 0)
+					if (serverSocket1.Create(80, SOCK_STREAM, NULL) == 0)
 					{
 						cout << "Tao socket that bai" << endl;
 						cout << serverSocket1.GetLastError();
@@ -97,26 +97,102 @@ int main(int argc, char *argv[])
 							cout << "Da co Client ket Noi !!!" << endl << endl;
 							//con2.Receive(&msgrcv, 100000, 0);
 							con2.Receive(&msgrcv, 100000, 0);
-							if (strstr(msgrcv, "Username=admin&Password=admin") != NULL)
+							//cout << msgrcv;
+							if (strstr(msgrcv,"POST") != NULL && strstr(msgrcv, "username=admin&password=admin") != NULL)
 							{
-								char msgs2[] = "HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:8080/info.html\r\n\r\n";
-								con2.Send(&msgs2, strlen(msgs2), 0);
-								con2.Receive(&msgrcv, 100000, 0);
-								ifstream info("info.html", ios::out | ios::binary | ios::ate);
-								int size = info.tellg();
-								char msgs3[10000] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: 10000\r\n\r\n";
-								int length = strlen(msgs3);
-								char* buff = new char[size];
-								info.seekg(0, ios::beg);
-								info.read(buff, size);
-								strcat(msgs3, buff);
-								con2.Send(&msgs3, length + size, 0);
-								info.close();
-								delete[]buff;
-								//cout << msgrcv;
+								char msgs2[] = "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html";
+								int sizem = strlen(msgs2);
+								con2.Send(&msgs2, sizem, 0);
+								con2.Close();
+								serverSocket1.Close();
+								if (serverSocket2.Create(80, SOCK_STREAM, NULL) == 0)
+								{
+									cout << "Tao socket that bai" << endl;
+									cout << serverSocket2.GetLastError();
+									return 1;
+								}
+								else
+								{
+									cout << "Thanh cong tao socket" << endl;
+									//Lang nghe
+									if (serverSocket2.Listen(5) == 0)
+									{
+										cout << "Khong the lang nghe.";
+										serverSocket2.Close();
+										return 1;
+									}
+									if (serverSocket2.Accept(con3))
+									{
+										cout << "Da co Client ket Noi !!!" << endl << endl;
+										while (strstr(msgrcv, "GET /info.html HTTP/1.1") == NULL)
+										{
+											con3.Receive(&msgrcv, 100000, 0);
+										}
+										ifstream info("info.html", ios::out | ios::binary | ios::ate);
+										int size = info.tellg();
+										char msgs3[10000] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: 10000\r\n\r\n";
+										int length = strlen(msgs3);
+										char* buff = new char[size];
+										info.seekg(0, ios::beg);
+										info.read(buff, size);
+										strcat(msgs3, buff);
+										con3.Send(&msgs3, length + size, 0);
+										info.close();
+										delete[]buff;
+										//cout << msgrcv;
+										con3.Close();
+										serverSocket2.Close();
+									}
+								}
 							}
-							con2.Close();
-							serverSocket1.Close();
+							else if (strstr(msgrcv,"POST") != NULL)
+							{
+								char msgs2[] = "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html";
+								int sizem = strlen(msgs2);
+								con2.Send(&msgs2, sizem, 0);
+								con2.Close();
+								serverSocket1.Close();
+								if (serverSocket2.Create(80, SOCK_STREAM, NULL) == 0)
+								{
+									cout << "Tao socket that bai" << endl;
+									cout << serverSocket2.GetLastError();
+									return 1;
+								}
+								else
+								{
+									cout << "Thanh cong tao socket" << endl;
+									//Lang nghe
+									if (serverSocket2.Listen(5) == 0)
+									{
+										cout << "Khong the lang nghe.";
+										serverSocket2.Close();
+										return 1;
+									}
+									if (serverSocket2.Accept(con3))
+									{
+										cout << "Da co Client ket Noi !!!" << endl << endl;
+										while (strstr(msgrcv, "GET /404.html HTTP/1.1") == NULL)
+										{
+											con3.Receive(&msgrcv, 100000, 0);
+										}
+										ifstream file404("404.html", ios::out | ios::binary | ios::ate);
+										int size = file404.tellg();
+										char msgs3[10000] = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Lenghth: 10000\r\n\r\n";
+										int length = strlen(msgs3);
+										char* buff = new char[size];
+										file404.seekg(0, ios::beg);
+										file404.read(buff, size);
+										strcat(msgs3, buff);
+										con3.Send(&msgs3, length + size, 0);
+										file404.close();
+										delete[]buff;
+										//cout << msgrcv;
+										con3.Close();
+										serverSocket2.Close();
+									}
+								}
+							}
+
 						}
 					}
 					
