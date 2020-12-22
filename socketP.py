31fd3228151a5bc90407426f0bafe539af8c3da0
Tranmyhan <67772342+Tranmyhan@users.noreply.github.com>
2020-12-22 10:39:47 +0700

    Update socketP.py

--- socketP.py
+++ socketP.py
@@ -13,7 +13,7 @@ def createServer():
     return server, 0
 
 def printRequest(request):
-    print("-------------HTTP request:\n")
+    print("-------------HTTP request:")
     if "GET / HTTP/1.1" in request:
         print("GET / HTTP/1.1")
     if "GET /index.html HTTP/1.1" in request:
@@ -26,93 +26,97 @@ def printRequest(request):
         print("GET /lam.jpg HTTP/1.1")
     if "GET /han.jpg HTTP/1.1" in request:
         print("GET /han.jpg HTTP/1.1")
+    if "POST / HTTP/1.1" in request:
+        print("POST / HTTP/1.1")
+    if "GET /download/han.jpg HTTP/1.1" in request:
+        print("GET /download/han.jpg HTTP/1.1")
 
-def printRespone(respone):
-    print("-------------HTTP respone:\n")
-    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/index.html" in respone:
+def printResponse(response):
+    print("-------------HTTP response:")
+    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/index.html" in response:
         print("HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/index.html")
-    if "HTTP/1.1 200 OK" in respone:
+    if "HTTP/1.1 200 OK" in response:
         print("HTTP/1.1 200 OK")
-    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html" in respone:
+    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html" in response:
         print("HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html")
-    if "HTTP/1.1 404 Not Found" in respone:
+    if "HTTP/1.1 404 Not Found" in response:
         print("HTTP/1.1 404 Not Found")
-    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html" in respone:
+    if "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html" in response:
         print("HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html")
 
 def sendMoveIndex(server,connector):
-    respone= "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/index.html"
-    printRespone(respone)
-    size=len(respone)
-    connector.send(bytes(respone,"utf-8"))
+    response= "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/index.html"
+    printResponse(response)
+    size=len(response)
+    connector.send(bytes(response,"utf-8"))
     connector.close()
     server.close()
 
 def sendIndex(server,connector):
     index = open ("index.html", "rb")
     buffer=index.read()
-    respone="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"%len(buffer)
-    printRespone(respone)
-    respone+=buffer.decode()    
-    connector.send(bytes(respone,"utf-8"))
+    response="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"%len(buffer)
+    printResponse(response)
+    response+=buffer.decode()    
+    connector.send(bytes(response,"utf-8"))
     index.close()
     connector.close()
     server.close()
 
 def sendMove404(server,connector):
-    respone="HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html"
-    printRespone(respone)
-    size=len(respone)
-    connector.send(bytes(respone,"utf-8"))
+    response="HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/404.html"
+    printResponse(response)
+    size=len(response)
+    connector.send(bytes(response,"utf-8"))
     connector.close()
     server.close()
 
 def send404(server,connector):
     index = open ("404.html", "rb")
     buffer=index.read()
-    respone="HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"%len(buffer)
-    printRespone(respone)
-    respone+=buffer.decode()    
-    connector.send(bytes(respone,"utf-8"))
+    response="HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"%len(buffer)
+    printResponse(response)
+    response+=buffer.decode()    
+    connector.send(bytes(response,"utf-8"))
     index.close()
     connector.close()
     server.close()
 
 def sendMoveFiles(server,connector):
-    respone="HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/files.html"
-    printRespone(respone)
-    size=len(respone)
-    connector.send(bytes(respone,"utf-8"))
+    response="HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/files.html"
+    printResponse(response)
+    size=len(response)
+    connector.send(bytes(response,"utf-8"))
     connector.close()
     server.close()
 
 def sendFiles(server,connector):
     index = open ("files.html", "rb")
     buffer=index.read()
-    respone="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
-    printRespone(respone)
-    respone+=buffer.decode()    
-    connector.send(bytes(respone,"utf-8"))
+    response="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
+    printResponse(response)
+    response+=buffer.decode()    
+    connector.send(bytes(response,"utf-8"))
     index.close()
     connector.close()
     server.close()
 
+
 def sendMoveInfo(server,connector):
-    respone= "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html"
-    printRespone(respone)
-    size=len(respone)
-    connector.send(bytes(respone,"utf-8"))
+    response= "HTTP/1.1 301 Moved Permanently\r\nLocation: http://localhost:80/info.html"
+    printResponse(response)
+    size=len(response)
+    connector.send(bytes(response,"utf-8"))
     connector.close()
     server.close()
-
 
 def sendInfo(server,connector):
     index = open ("info.html", "rb")
     buffer=index.read()
-    respone="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
-    printRespone(respone)
-    respone+=buffer.decode()    
-    connector.send(bytes(respone,"utf-8"))
+    response="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
+    printResponse(response)
+    response+=buffer.decode()    
+    connector.send(bytes(response,"utf-8"))
     index.close()
     connector.close()
     server.close()
@@ -143,19 +147,55 @@ def sendInfo(server,connector):
 def sendImg(server,connector,img):
     index = open (img, "rb")
     buffer=index.read()
-    respone="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
-    printRespone(respone)
-    respone =bytes(respone,"utf-8") +buffer
-    connector.send(respone)
+    response="""HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
+    printResponse(response)
+    response =bytes(response,"utf-8") +buffer
+    connector.send(response)
     index.close()
 
+def sendFaviconIcon(server, connector):
+    index = open("favicon.ico", "rb")
+    buffer=index.read()
+    response="""HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Lenghth: %d\r\n\r\n"""%len(buffer)
+    printResponse(response)
+    response+=buffer.decode()    
+    connector.send(bytes(response,"utf-8"))
+    index.close()
+    connector.close()
+    server.close()
+
+def MenuDownload(server, connector, request):
+    if "GET /download/han.jpg HTTP/1.1" in request:
+        file = open("download/han.jpg","rb")
+        response="HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nTransfer-Encoding: chunked\r\n\r\n"
+    if "GET /download/lam.jpg HTTP/1.1" in request:
+        file = open("download/lam.jpg","rb")
+        response="HTTP/1.1 200 OK\r\nContent-Type: image/jpg\r\nTransfer-Encoding: chunked\r\n\r\n"
+    if "GET /download/Project_Socket_Programming_112020.pdf HTTP/1.1" in request:
+        file = open("download/Project_Socket_Programming_112020.pdf","rb")
+        response="HTTP/1.1 200 OK\r\nContent-Type: application/pdf\r\nTransfer-Encoding: chunked\r\n\r\n"
+
+    printResponse(response)
+    response=response.encode()
+    CHUNK_SIZE=1000
+    while True:
+        chunk=file.read(CHUNK_SIZE)
+        size=len(chunk)
+        if not chunk:
+            break
+        response +=hex(size)[2:].encode()+"\r\n".encode()+chunk+"\r\n".encode()
+    response+="0\r\n\r\n".encode()
+    connector.send(response)
+    file.close()
+    connector.close()
+    server.close()
 
 def main():
     server, r = createServer()
-    print("Server: \n")
-    print("IP: localhost\n")
-    print("Port: 80\n")
-    print("Waiting for Client.\n")
+    print("Server:")
+    print("IP: localhost")
+    print("Port: 80")
+    print("Waiting for Client.")
     print("Please enter 'localhost' or 'localhost/index.html' into browser to connect to the server.\n")
     connector, address = server.accept()
     if connector:
@@ -211,13 +251,36 @@ def main():
             connector, address = server.accept()
             if(connector):
                 request = connector.recv(1024).decode()
-                if "GET /index.html HTTP/1.1" in request:
-                    printRequest(request)
-                    sendIndex(server,connector)
-                else:
-                   if "GET /files.html HTTP/1.1" in request:
-                        printRequest(request)
-                        sendFiles(server,connector)
+                if "GET /favicon.ico HTTP/1.1" in request:
+                    sendFaviconIcon(server,connector)
+                    server, r = createServer()
+                    if r==1:
+                        return 1
+                    connector, address = server.accept()
+                    if(connector):
+                        request = connector.recv(1024).decode()
+                        if "GET /index.html HTTP/1.1" in request:
+                            printRequest(request)
+                            sendIndex(server,connector)
+                        elif "GET /files.html HTTP/1.1" in request:
+                            printRequest(request)                            
+                            sendFiles(server,connector)
+                            server, r = createServer()
+                            if r==1:
+                                return 1
+                            connector, address = server.accept()
+                            if(connector):
+                                request = connector.recv(1024).decode()
+                                while not "GET /index.html HTTP/1.1" in request:
+                                    MenuDownload(server,connector,request)
+                                    server, r = createServer()
+                                    if r==1:
+                                        return 1
+                                    connector, address = server.accept()
+                                    if(connector):
+                                        request = connector.recv(1024).decode()
+                                printRequest(request)
+                                sendIndex(server,connector)
 
         else:
             if "POST" not in request or  "username=admin&password=admin" not in request:
